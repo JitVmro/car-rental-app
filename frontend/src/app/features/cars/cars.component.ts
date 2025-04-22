789489611abf14d0c42e3e026fb4ba4615387336
Swetaparna Mishra <swetaparna_mishra@epam.com>
2025-04-22 18:59:04 +0530

    Minor Changes to Cars Page

--- frontend/src/app/features/cars/cars.component.ts
+++ frontend/src/app/features/cars/cars.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { CarFilterComponent } from "../car-filter/car-filter.component";
 import { CardsComponent } from "../cards/cards.component";
-import { AboutUsComponent } from "../about-us/about-us.component";
-import { ReviewSliderComponent } from "../review-slider/review-slider.component";
-import { FaqComponent } from "../faq/faq.component";
+
 
 @Component({
   selector: 'app-cars',
-  imports: [CarFilterComponent, CardsComponent, AboutUsComponent, ReviewSliderComponent, FaqComponent],
+  imports: [CarFilterComponent, CardsComponent],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
