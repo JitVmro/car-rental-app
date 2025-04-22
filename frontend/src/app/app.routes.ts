0ed0c4519588fcdc17fcde7f6fa9ac95dbab4e9f
Swetaparna Mishra <swetaparna_mishra@epam.com>
2025-04-22 13:01:29 +0530

    header-component added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CarsComponent } from './features/cars/cars.component';
+import { BookingsComponent } from './features/bookings/bookings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'cars',component:CarsComponent},
+    {path:'bookings',component:BookingsComponent}
+];
