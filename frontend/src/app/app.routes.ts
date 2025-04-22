d3ac23e63d7c9ee3116ab28fcf8e99bc7b1d7927
Swetaparna Mishra <swetaparna_mishra@epam.com>
2025-04-22 14:08:55 +0530

    Added Routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CarsComponent } from './features/cars/cars.component';
 import { BookingsComponent } from './features/bookings/bookings.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
+    {path:'',component:HomeComponent},
     {path: 'cars',component:CarsComponent},
     {path:'bookings',component:BookingsComponent}
 ];
