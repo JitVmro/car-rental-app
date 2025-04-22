5e53d695abafd4a79daba4617ce7db1f9668f6e5
Swetaparna Mishra <swetaparna_mishra@epam.com>
2025-04-22 18:47:18 +0530

    Added Components to Cars Route

--- frontend/src/app/features/cars/cars.component.ts
+++ frontend/src/app/features/cars/cars.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { CarFilterComponent } from "../car-filter/car-filter.component";
+import { CardsComponent } from "../cards/cards.component";
+import { AboutUsComponent } from "../about-us/about-us.component";
+import { ReviewSliderComponent } from "../review-slider/review-slider.component";
+import { FaqComponent } from "../faq/faq.component";
 
 @Component({
   selector: 'app-cars',
-  imports: [],
+  imports: [CarFilterComponent, CardsComponent, AboutUsComponent, ReviewSliderComponent, FaqComponent],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
