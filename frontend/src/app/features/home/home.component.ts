56b8bb3070ce5cf472195a87eada2738028bb065
Swetaparna Mishra <swetaparna_mishra@epam.com>
2025-04-23 10:19:54 +0530

    Integrated Map Component

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -6,13 +6,14 @@ import { AboutUsComponent } from "../about-us/about-us.component";
 import { ReviewSliderComponent } from "../review-slider/review-slider.component";
 import { FaqComponent } from "../faq/faq.component";
 import { CarDetailComponent } from "../../shared/car-detail/car-detail.component";
+import { MapComponent } from "../map/map.component";
 
 @Component({
   selector: 'app-home',
   
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [CarFilterComponent, CardsComponent, AboutUsComponent, ReviewSliderComponent, FaqComponent, CarDetailComponent]
+  imports: [CarFilterComponent, CardsComponent, AboutUsComponent, ReviewSliderComponent, FaqComponent, CarDetailComponent, MapComponent]
 })
 export class HomeComponent {
 
