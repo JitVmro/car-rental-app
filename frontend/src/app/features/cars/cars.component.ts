735deedf085fa5ceda8f37893010b433aa911c7f
Vaibhav Lohitashv <vaibhav_lohitashv@epam.com>
2025-04-24 17:01:25 +0530

    Minor import fixing

--- frontend/src/app/features/cars/cars.component.ts
+++ frontend/src/app/features/cars/cars.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CarFilterComponent } from "../car-filter/car-filter.component";
 import { CardsComponent } from "../cards/cards.component";
+import { CarDetailComponent } from '../../shared/car-detail/car-detail.component';
 
 
 
 @Component({
   selector: 'app-cars',
-  imports: [CarFilterComponent, CardsComponent],
+  imports: [CarFilterComponent, CardsComponent, CarDetailComponent],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
