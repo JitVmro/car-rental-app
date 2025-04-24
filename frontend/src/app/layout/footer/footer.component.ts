ee17688ae7671ed1f167b9f53cbc722d269ef113
Swetaparna Mishra <swetaparna_mishra@epam.com>
2025-04-24 16:14:21 +0530

    Added Navigation to logo and footer

--- frontend/src/app/layout/footer/footer.component.ts
+++ frontend/src/app/layout/footer/footer.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth/auth-service.service';
+import { RouterModule, Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [NgIf],
+  imports: [NgIf,RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
   isLoggedIn: boolean;
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,public router: Router) {
     this.isLoggedIn = this.authService.isLoggedIn();
   }
 }
